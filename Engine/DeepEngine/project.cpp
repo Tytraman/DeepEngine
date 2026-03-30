aa65f197b0dc7134dd27de0d005e8b1782584735
Tytraman <tytraman@gmail.com>
2026-03-30 15:55:20 +0200

    add ui_border_size to project settings

--- Engine/DeepEngine/project.cpp
+++ Engine/DeepEngine/project.cpp
@@ -7,6 +7,17 @@
 
 namespace deep
 {
+    static const char *WORLD            = "world";
+    static const char *UI               = "UI";
+    static const char *BACKGROUND_COLOR = "background_color";
+    static const char *BORDER_COLOR     = "border_color";
+    static const char *TEXT_COLOR       = "text_color";
+    static const char *R                = "R";
+    static const char *G                = "G";
+    static const char *B                = "B";
+    static const char *A                = "A";
+    static const char *BORDER_SIZE      = "border_size";
+
     project::project(const ref<ctx> &context) noexcept
             : object(context)
     {
@@ -60,33 +71,31 @@ namespace deep
         }
         current_proj->m_settings["version"] = "0.0.1";
 
-        current_proj->m_settings["world"]["background_color"] = {
-            { "R", 0.0f },
-            { "G", 0.0f },
-            { "B", 0.0f }
+        current_proj->m_settings[WORLD][BACKGROUND_COLOR] = {
+            { R, 0.0f },
+            { G, 0.0f },
+            { B, 0.0f }
         };
 
-        constexpr const char *UI = "UI";
-
-        current_proj->m_settings[UI]["border_color"] = {
-            { "R", imgui_manager::DefaultGlobalBorderColor.x * 255.0f },
-            { "G", imgui_manager::DefaultGlobalBorderColor.y * 255.0f },
-            { "B", imgui_manager::DefaultGlobalBorderColor.z * 255.0f },
-            { "A", imgui_manager::DefaultGlobalBorderColor.w * 255.0f }
+        current_proj->m_settings[UI][BORDER_COLOR] = {
+            { R, imgui_manager::DefaultGlobalBorderColor.x * 255.0f },
+            { G, imgui_manager::DefaultGlobalBorderColor.y * 255.0f },
+            { B, imgui_manager::DefaultGlobalBorderColor.z * 255.0f },
+            { A, imgui_manager::DefaultGlobalBorderColor.w * 255.0f }
         };
 
-        current_proj->m_settings[UI]["background_color"] = {
-            { "R", imgui_manager::DefaultGlobalBackgroundColor.x * 255.0f },
-            { "G", imgui_manager::DefaultGlobalBackgroundColor.y * 255.0f },
-            { "B", imgui_manager::DefaultGlobalBackgroundColor.z * 255.0f },
-            { "A", imgui_manager::DefaultGlobalBackgroundColor.w * 255.0f }
+        current_proj->m_settings[UI][BACKGROUND_COLOR] = {
+            { R, imgui_manager::DefaultGlobalBackgroundColor.x * 255.0f },
+            { G, imgui_manager::DefaultGlobalBackgroundColor.y * 255.0f },
+            { B, imgui_manager::DefaultGlobalBackgroundColor.z * 255.0f },
+            { A, imgui_manager::DefaultGlobalBackgroundColor.w * 255.0f }
         };
 
-        current_proj->m_settings[UI]["text_color"] = {
-            { "R", imgui_manager::DefaultGlobalTextColor.x * 255.0f },
-            { "G", imgui_manager::DefaultGlobalTextColor.y * 255.0f },
-            { "B", imgui_manager::DefaultGlobalTextColor.z * 255.0f },
-            { "A", imgui_manager::DefaultGlobalTextColor.w * 255.0f }
+        current_proj->m_settings[UI][TEXT_COLOR] = {
+            { R, imgui_manager::DefaultGlobalTextColor.x * 255.0f },
+            { G, imgui_manager::DefaultGlobalTextColor.y * 255.0f },
+            { B, imgui_manager::DefaultGlobalTextColor.z * 255.0f },
+            { A, imgui_manager::DefaultGlobalTextColor.w * 255.0f }
         };
 
         usize bytes_written;
@@ -167,30 +176,136 @@ namespace deep
             return;
         }
 
-        constexpr const char *WORLD            = "world";
-        constexpr const char *UI               = "UI";
-        constexpr const char *BACKGROUND_COLOR = "background_color";
-        constexpr const char *BORDER_COLOR     = "border_color";
-        constexpr const char *TEXT_COLOR       = "text_color";
+        float world_r = 0.0f;
+        float world_g = 0.0f;
+        float world_b = 0.0f;
 
-        float world_r = m_settings[WORLD][BACKGROUND_COLOR]["R"];
-        float world_g = m_settings[WORLD][BACKGROUND_COLOR]["G"];
-        float world_b = m_settings[WORLD][BACKGROUND_COLOR]["B"];
+        if (m_settings.contains(WORLD))
+        {
+            json &world = m_settings[WORLD];
 
-        float ui_border_color_R = m_settings[UI][BORDER_COLOR]["R"];
-        float ui_border_color_G = m_settings[UI][BORDER_COLOR]["G"];
-        float ui_border_color_B = m_settings[UI][BORDER_COLOR]["B"];
-        float ui_border_color_A = m_settings[UI][BORDER_COLOR]["A"];
+            if (world.contains(BACKGROUND_COLOR))
+            {
+                json &world_background_color = world[BACKGROUND_COLOR];
 
-        float ui_background_color_R = m_settings[UI][BACKGROUND_COLOR]["R"];
-        float ui_background_color_G = m_settings[UI][BACKGROUND_COLOR]["G"];
-        float ui_background_color_B = m_settings[UI][BACKGROUND_COLOR]["B"];
-        float ui_background_color_A = m_settings[UI][BACKGROUND_COLOR]["A"];
+                if (world_background_color.contains(R))
+                {
+                    world_r = world_background_color[R];
+                }
 
-        float ui_text_color_R = m_settings[UI][TEXT_COLOR]["R"];
-        float ui_text_color_G = m_settings[UI][TEXT_COLOR]["G"];
-        float ui_text_color_B = m_settings[UI][TEXT_COLOR]["B"];
-        float ui_text_color_A = m_settings[UI][TEXT_COLOR]["A"];
+                if (world_background_color.contains(G))
+                {
+                    world_g = world_background_color[G];
+                }
+
+                if (world_background_color.contains(B))
+                {
+                    world_b = world_background_color[B];
+                }
+            }
+        }
+
+        float ui_border_color_R = imgui_manager::DefaultGlobalBorderColor.x;
+        float ui_border_color_G = imgui_manager::DefaultGlobalBorderColor.y;
+        float ui_border_color_B = imgui_manager::DefaultGlobalBorderColor.z;
+        float ui_border_color_A = imgui_manager::DefaultGlobalBorderColor.w;
+
+        float ui_background_color_R = imgui_manager::DefaultGlobalBackgroundColor.x;
+        float ui_background_color_G = imgui_manager::DefaultGlobalBackgroundColor.y;
+        float ui_background_color_B = imgui_manager::DefaultGlobalBackgroundColor.z;
+        float ui_background_color_A = imgui_manager::DefaultGlobalBackgroundColor.w;
+
+        float ui_text_color_R = imgui_manager::DefaultGlobalTextColor.x;
+        float ui_text_color_G = imgui_manager::DefaultGlobalTextColor.y;
+        float ui_text_color_B = imgui_manager::DefaultGlobalTextColor.z;
+        float ui_text_color_A = imgui_manager::DefaultGlobalTextColor.w;
+
+        float ui_border_size = imgui_manager::DefaultBorderSize;
+
+        if (m_settings.contains(UI))
+        {
+            json &ui = m_settings[UI];
+
+            if (ui.contains(BORDER_COLOR))
+            {
+                json &ui_border_color = ui[BORDER_COLOR];
+
+                if (ui_border_color.contains(R))
+                {
+                    ui_border_color_R = ui_border_color[R];
+                }
+
+                if (ui_border_color.contains(G))
+                {
+                    ui_border_color_G = ui_border_color[G];
+                }
+
+                if (ui_border_color.contains(B))
+                {
+                    ui_border_color_B = ui_border_color[B];
+                }
+
+                if (ui_border_color.contains(A))
+                {
+                    ui_border_color_A = ui_border_color[A];
+                }
+            }
+
+            if (ui.contains(BACKGROUND_COLOR))
+            {
+                json &ui_background_color = ui[BACKGROUND_COLOR];
+
+                if (ui_background_color.contains(R))
+                {
+                    ui_background_color_R = ui_background_color[R];
+                }
+
+                if (ui_background_color.contains(G))
+                {
+                    ui_background_color_G = ui_background_color[G];
+                }
+
+                if (ui_background_color.contains(B))
+                {
+                    ui_background_color_B = ui_background_color[B];
+                }
+
+                if (ui_background_color.contains(A))
+                {
+                    ui_background_color_A = ui_background_color[A];
+                }
+            }
+
+            if (ui.contains(TEXT_COLOR))
+            {
+                json &ui_text_color = ui[TEXT_COLOR];
+
+                if (ui_text_color.contains(R))
+                {
+                    ui_text_color_R = ui_text_color[R];
+                }
+
+                if (ui_text_color.contains(G))
+                {
+                    ui_text_color_G = ui_text_color[G];
+                }
+
+                if (ui_text_color.contains(B))
+                {
+                    ui_text_color_B = ui_text_color[B];
+                }
+
+                if (ui_text_color.contains(A))
+                {
+                    ui_text_color_A = ui_text_color[A];
+                }
+            }
+
+            if (ui.contains(BORDER_SIZE))
+            {
+                ui_border_size = ui[BORDER_SIZE];
+            }
+        }
 
         graph->set_background_color(fvec4(
                 world_r / 255.0f,
@@ -215,6 +330,8 @@ namespace deep
                 ui_text_color_G / 255.0f,
                 ui_text_color_B / 255.0f,
                 ui_text_color_A / 255.0f));
+
+        im_manager->set_global_border_size(ui_border_size);
     }
 
     bool project::save_settings(const ref<engine> &eng) noexcept
@@ -241,44 +358,41 @@ namespace deep
             return false;
         }
 
-        constexpr const char *WORLD            = "world";
-        constexpr const char *UI               = "UI";
-        constexpr const char *BACKGROUND_COLOR = "background_color";
-        constexpr const char *BORDER_COLOR     = "border_color";
-        constexpr const char *TEXT_COLOR       = "text_color";
-
         fvec4 world_background_color = graph->get_background_color();
 
         ImVec4 ui_border_color     = im_manager->get_global_border_color();
         ImVec4 ui_background_color = im_manager->get_global_background_color();
         ImVec4 ui_text_color       = im_manager->get_global_text_color();
+        float ui_border_size       = im_manager->get_global_border_size();
 
         m_settings[WORLD][BACKGROUND_COLOR] = {
-            { "R", world_background_color.x * 255.0f },
-            { "G", world_background_color.y * 255.0f },
-            { "B", world_background_color.z * 255.0f }
+            { R, world_background_color.x * 255.0f },
+            { G, world_background_color.y * 255.0f },
+            { B, world_background_color.z * 255.0f }
         };
 
         m_settings[UI][BORDER_COLOR] = {
-            { "R", ui_border_color.x * 255.0f },
-            { "G", ui_border_color.y * 255.0f },
-            { "B", ui_border_color.z * 255.0f },
-            { "A", ui_border_color.w * 255.0f }
+            { R, ui_border_color.x * 255.0f },
+            { G, ui_border_color.y * 255.0f },
+            { B, ui_border_color.z * 255.0f },
+            { A, ui_border_color.w * 255.0f }
         };
 
         m_settings[UI][BACKGROUND_COLOR] = {
-            { "R", ui_background_color.x * 255.0f },
-            { "G", ui_background_color.y * 255.0f },
-            { "B", ui_background_color.z * 255.0f },
-            { "A", ui_background_color.w * 255.0f }
+            { R, ui_background_color.x * 255.0f },
+            { G, ui_background_color.y * 255.0f },
+            { B, ui_background_color.z * 255.0f },
+            { A, ui_background_color.w * 255.0f }
         };
 
         m_settings[UI][TEXT_COLOR] = {
-            { "R", ui_text_color.x * 255.0f },
-            { "G", ui_text_color.y * 255.0f },
-            { "B", ui_text_color.z * 255.0f },
-            { "A", ui_text_color.w * 255.0f }
+            { R, ui_text_color.x * 255.0f },
+            { G, ui_text_color.y * 255.0f },
+            { B, ui_text_color.z * 255.0f },
+            { A, ui_text_color.w * 255.0f }
         };
+
+        m_settings[UI][BORDER_SIZE] = ui_border_size;
 
         m_settings_stream->set_length(0);
         m_settings_stream->seek(0, stream::seek_origin::Begin);
